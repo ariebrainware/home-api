3c84f1602c2cbacfc709c7c0afbbe4d8c90f5795
Arie Brainware <publicu001@protonmail.com>
2019-07-14 13:59:58 +0700

    Update root endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ router.get('/', (req, res) => {
   res.status(200).send([
     {
       project: 'Ticket Xpo',
-      url: 'https://api.ariebrainware.com/xpo/v1',
+      url: 'https://api.ariebrainware.com/v1/xpo',
     },
     {
       project: 'Galonku API',
-      url: 'https://api.ariebrainware.com/galonku/v1',
+      url: 'https://api.ariebrainware.com/v1/galonku',
+    },
+    {
+      project: 'Paylist API',
+      url: 'https://api.ariebrainware.com/v1/paylist'
     }
   ])
 })
