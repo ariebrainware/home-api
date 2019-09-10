b717ad9bf4869860154bd240b467bbeb72e8bcc9
Arie Brainware <publicu001@protonmail.com>
2019-09-10 21:21:16 +0700

    Update dependencies and main route

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ router.get('/', (req, res) => {
   res.status(200).send([
     {
       project: 'Ticket Xpo',
-      url: 'https://api.ariebrainware.com/v1/xpo',
+      url: 'https://api.ariebrainware.com/xpo/v1',
     },
     {
       project: 'Galonku API',
-      url: 'https://api.ariebrainware.com/v1/galonku',
+      url: 'https://api.ariebrainware.com/galonku/v1',
     },
     {
       project: 'Paylist API',
-      url: 'https://api.ariebrainware.com/v1/paylist'
+      url: 'https://api.ariebrainware.com/paylist/v1'
     }
   ])
 })
